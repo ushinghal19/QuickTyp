9eb8fd56f55c2c518dbc7bf1e1e49f36c294ba45
Utsav Shinghal <u.shinghal@mail.utoronto.ca>
2020-01-19 17:30:04 -0500

    first push

--- TypeRacer1.0/GameViewController.swift
+++ TypeRacer1.0/GameViewController.swift
@@ -10,26 +10,129 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var progressView:UIProgressView?
+    @IBOutlet weak var text:UILabel!
+    @IBOutlet weak var inputField:UITextField?
+    @IBOutlet weak var timeLabel:UILabel?
+    @IBOutlet weak var wordCounter: UILabel?
+    @IBOutlet weak var restartButton: UIButton?
+    @IBOutlet weak var highScore: UILabel?
+    
+    var pinkCounter = 0
+    var timer: Timer?
+    var seconds = 60
+    let progress = Progress(totalUnitCount: 10)
+    var high_score = 0
+    
+    func updateTimeLabel() {
+        let min = (seconds / 60) % 60
+        let sec = seconds % 60
+        
+        timeLabel?.text = String(format: "%02d", min) + ":" + String(format: "%02d", sec)
+    }
+    
+//    func updateProgress() {
+//        progressView?.progress += 1
+//    }
+    
+    func updateWordCounter() {
+        wordCounter?.text = String(pinkCounter)
+        pinkCounter += 1
+        
+    }
+    
+    func randomWord() {
+        text.text = String.randomWord()
+    }
+    
+    func updateHighScore() {
+        if pinkCounter >= high_score {
+            high_score = pinkCounter
+            highScore?.text = String(high_score)
+        }
+    }
+
+    @IBAction func restartGame(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
+        timeLabel?.textColor = UIColor.white
+        seconds = 60
+        updateTimeLabel()
+        //updateProgress()
+        randomWord()
+        pinkCounter = 0
+        updateHighScore()
+        updateWordCounter()
+        
+    }
+    
+    
+    @IBAction func inputFieldDidChange()
+    {
+        guard let strText = text.text, let inputText = inputField?.text
+        else {
+            return
+        }
+        
+        guard inputText.count == strText.count else {
+            return
+        }
+
+        var isCorrect = true
+        let input = inputText
+        let string = strText
+        if input != string {
+            isCorrect = false
+        }
+        
+        if isCorrect{
+            updateHighScore()
+            updateWordCounter()
+            randomWord()
+            //updateProgress()
+            inputField?.text = ""
+            
+
+        }
+        else{
+            
+        }
+        
+        
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                
+                if self.seconds == 0 {
+                    self.finishGame()
+                } else if self.seconds <= 60 {
+                    self.seconds -= 1
+                    self.updateTimeLabel()
+                    if self.seconds <= 10 && self.seconds % 2 == 0 {
+                        self.timeLabel?.textColor = UIColor.red
+                    }
+                    if self.seconds <= 10 && self.seconds % 2 == 1 {
+                        self.timeLabel?.textColor = UIColor.white
+                    }
+                    if self.seconds == 0{
+                        self.timeLabel?.textColor = UIColor.white
+                    }
+                }
+                }
+            }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        updateHighScore()
+        updateWordCounter()
+        randomWord()
+        //updateProgress()
+        updateTimeLabel()
+        
+        
     }
 
     override var shouldAutorotate: Bool {
@@ -46,5 +149,26 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func finishGame()
+    {
+        timer?.invalidate()
+        timer = nil
+        let alert = UIAlertController(title:
+            "Time's Up!", message: "Your time is up! You got \(pinkCounter-1) Words Per Minute. Awesome!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK, start new game", style:.default, handler:nil))
+        self.present(alert, animated:true, completion:nil)
+        
+        progressView?.progress = 0.0
+        seconds = 60
+        updateTimeLabel()
+        //updateProgress()
+        randomWord()
+        pinkCounter = 0
+        updateHighScore()
+        updateWordCounter()
+        
+        
     }
 }
